15ab7b21377d11664caa03b28e94a3bfd4433ec0
Artem Malyi <artem.malyi@gmail.com>
2017-12-13 01:08:12 +0200

    sending all the packets through specified network interface card

--- connector.py
+++ connector.py
@@ -40,6 +40,7 @@ from scapy.all import *
 from scapy.layers.inet import *
 from scapy.layers.bgp import *
 
+outNic = "ens38"
 myIp   = "10.10.10.2"
 peerIp = "10.10.10.1" #"170.104.164.236"
 srcPort = random.randint(40000, 65535)
@@ -69,16 +70,16 @@ def handlePackets(p):
     if p[TCP].flags == 18 and not handshakeOk: 
         print "\n[+] got SYN+ACK packet:", p.summary()
         # got a SYN+ACK, note the peer's seq to use later in BGPOpen and send an ACK now
-        ack = IP(dst=p[IP].src, id=int(RandShort())) / TCP(sport=p[TCP].dport, dport=p[TCP].sport, ack=p[TCP].seq+1, seq=p[TCP].ack, flags="A")
+        ack = Ether() / IP(dst=p[IP].src, id=int(RandShort())) / TCP(sport=p[TCP].dport, dport=p[TCP].sport, ack=p[TCP].seq+1, seq=p[TCP].ack, flags="A")
         print "[i] sending ACK packet:", ack.summary()
-        send(ack)	
+        sendp(ack, iface=outNic)	
         handshakeOk = True
         print "\n[+] completed TCP handshake with", peerIp
 
     if handshakeOk and not bgpOpenSent:
-        bgpOpen = IP(dst=p[IP].src, id=int(RandShort())) / TCP(sport=p[TCP].dport, dport=p[TCP].sport, ack=p[TCP].seq+1, seq=p[TCP].ack, flags="PA") / BGPHeader(type=1) / BGPOpen(version=4, AS=65002, hold_time=180, bgp_id=myIp)
+        bgpOpen = Ether() / IP(dst=p[IP].src, id=int(RandShort())) / TCP(sport=p[TCP].dport, dport=p[TCP].sport, ack=p[TCP].seq+1, seq=p[TCP].ack, flags="PA") / BGPHeader(type=1) / BGPOpen(version=4, AS=65002, hold_time=180, bgp_id=myIp)
         print "[i] sending BGPOPEN packet:", bgpOpen.summary()
-        send (bgpOpen)
+        sendp(bgpOpen, iface=outNic)
         bgpOpenSent = True
 
     if bgpOpenSent and p.haslayer(BGPOpen) and not firstKeepAliveSent:
@@ -86,25 +87,25 @@ def handlePackets(p):
         print "\n[+] got BGPOPEN from peer:", p.summary()
         pl = BGPHeader(p.getlayer(Raw).load)
         #print "type:", p[BGPHeader].type, "len1:", p[BGPHeader].len, "len2:", pl[BGPHeader].len
-        ack2 = IP(dst=p[IP].src, id=int(RandShort())) / TCP(sport=p[TCP].dport, dport=p[TCP].sport, ack=p[TCP].seq+p[BGPHeader].len+pl[BGPHeader].len, seq=p[TCP].ack, flags="A")
+        ack2 = Ether() / IP(dst=p[IP].src, id=int(RandShort())) / TCP(sport=p[TCP].dport, dport=p[TCP].sport, ack=p[TCP].seq+p[BGPHeader].len+pl[BGPHeader].len, seq=p[TCP].ack, flags="A")
         print "[i] sending ACK packet:", ack2.summary(), "len:", p[BGPHeader].len
-        send (ack2)
-        keepAlive = IP(dst=p[IP].src, id=int(RandShort())) / TCP(sport=p[TCP].dport, dport=p[TCP].sport, ack=p[TCP].seq+p[BGPHeader].len+pl[BGPHeader].len, seq=p[TCP].ack, flags="PA") / BGPHeader(type=4, len=19)
+        sendp(ack2, iface=outNic)
+        keepAlive = Ether() / IP(dst=p[IP].src, id=int(RandShort())) / TCP(sport=p[TCP].dport, dport=p[TCP].sport, ack=p[TCP].seq+p[BGPHeader].len+pl[BGPHeader].len, seq=p[TCP].ack, flags="PA") / BGPHeader(type=4, len=19)
         print "[i] sending first BGPKEEPALIVE packet:", keepAlive.summary()
-        send (keepAlive)
+        sendp(keepAlive, iface=outNic)
         firstKeepAliveSent = True
         return
 
     if p.haslayer(BGPHeader) and p[BGPHeader].type == 4:
         # got BGPKEEPALIVE, send keep alive and listen for peer's keep alive 
         print "\n[+] got BGPKEEPALIVE from peer:", p.summary(), "seq:", p[TCP].seq, "ack:", p[TCP].ack
-        ack2 = IP(dst=p[IP].src, id=int(RandShort())) / TCP(sport=p[TCP].dport, dport=p[TCP].sport, ack=p[TCP].seq+p[BGPHeader].len, seq=p[TCP].ack, flags="A")
+        ack2 = Ether() / IP(dst=p[IP].src, id=int(RandShort())) / TCP(sport=p[TCP].dport, dport=p[TCP].sport, ack=p[TCP].seq+p[BGPHeader].len, seq=p[TCP].ack, flags="A")
         print "[i] sending ACK packet:", ack2.summary()
-        send (ack2)
+        sendp(ack2, iface=outNic)
         #if not firstKeepAlive:
-        #	keepAlive = IP(dst=p[IP].src, id=int(RandShort())) / TCP(sport=p[TCP].dport, dport=p[TCP].sport, ack=p[TCP].seq+p[BGPHeader].len, seq=p[TCP].ack, flags="PA") / BGPHeader(type=4, len=19)
+        #	keepAlive = Ether() / IP(dst=p[IP].src, id=int(RandShort())) / TCP(sport=p[TCP].dport, dport=p[TCP].sport, ack=p[TCP].seq+p[BGPHeader].len, seq=p[TCP].ack, flags="PA") / BGPHeader(type=4, len=19)
         #	print "[i] sending BGPKEEPALIVE packet:", keepAlive.summary()
-        #	send (keepAlive)
+        #	sendp(keepAlive, iface=outNic)
         #firstKeepAlive = False
 
     if p.haslayer(BGPHeader) and p[BGPHeader].type == 3:
@@ -113,15 +114,15 @@ def handlePackets(p):
     if p.haslayer(BGPHeader) and p[BGPHeader].type == 2:
         print "\n[+] got BGPUPDATE from peer:", p.summary()
         #update={'TYPE':0,'ORIGIN':'\x00','NEXT_HOP':'\x0a\x0a\x0a\x02','MULTI_EXIT_DISC':'\x00\x00\x00\x00','LOCAL_PREF':'\x00\x00\x00\x96','NLRI':[(24, '192.168.2.0')],'AS_PATH':'65002'}
-        #updatePacket = IP(dst=p[IP].src, id=int(RandShort())) / TCP(sport=p[TCP].dport, dport=p[TCP].sport, ack=p[TCP].seq+p[BGPHeader].len, seq=p[TCP].ack, flags="PA") / Raw(load=update)
-        send(IP(dst=p[IP].src, id=int(RandShort())) / TCP(sport=p[TCP].dport, dport=p[TCP].sport, ack=p[TCP].seq+p[BGPHeader].len, seq=p[TCP].ack, flags="PA") /
-             BGPHeader(type=2) / BGPUpdate(nlri='192.168.2.0/24',
-                                           total_path=[BGPPathAttribute(type='ORIGIN', value='\x00'),
-                                                       BGPPathAttribute(type='NEXT_HOP', value='\x0a\x0a\x0a\x02'),
-                                                       BGPPathAttribute(flags=128L, type='MULTI_EXIT_DISC', value='\x00\x00\x00\x00'),
-                                                       BGPPathAttribute(type='LOCAL_PREF', value='\x00\x00\x00\x96'),
-                                                       BGPPathAttribute(type='AS_PATH', value=''),
-                                                      ]))			
+        #updatePacket = Ether() / IP(dst=p[IP].src, id=int(RandShort())) / TCP(sport=p[TCP].dport, dport=p[TCP].sport, ack=p[TCP].seq+p[BGPHeader].len, seq=p[TCP].ack, flags="PA") / Raw(load=update)
+        sendp(Ether() / IP(dst=p[IP].src, id=int(RandShort())) / TCP(sport=p[TCP].dport, dport=p[TCP].sport, ack=p[TCP].seq+p[BGPHeader].len, seq=p[TCP].ack, flags="PA") /
+              BGPHeader(type=2) / BGPUpdate(nlri='192.168.2.0/24',
+                                            total_path=[BGPPathAttribute(type='ORIGIN', value='\x00'),
+                                                        BGPPathAttribute(type='NEXT_HOP', value='\x0a\x0a\x0a\x02'),
+                                                        BGPPathAttribute(flags=128L, type='MULTI_EXIT_DISC', value='\x00\x00\x00\x00'),
+                                                        BGPPathAttribute(type='LOCAL_PREF', value='\x00\x00\x00\x96'),
+                                                        BGPPathAttribute(type='AS_PATH', value=''),
+                                                       ]), iface=outNic)			
         #print "[i] sending BGPUPDATE packet:", updatePacket.summary()
         #send(updatePacket)
 
@@ -144,9 +145,9 @@ def bgpConnect(ipAddr):
 
     print "[i] trying to connect to", ipAddr
 
-    syn = IP(dst=ipAddr, id=int(RandShort())) / TCP(sport=srcPort, dport=179, ack=0, seq=randomSeq, flags="S")
+    syn = Ether() / IP(dst=ipAddr, id=int(RandShort())) / TCP(sport=srcPort, dport=179, ack=0, seq=randomSeq, flags="S")
     print "[i] sending SYN packet:", syn.summary()
-    send(syn)
+    sendp(syn, iface=outNic)
 
     sniff(filter="tcp", stop_filter=stopFilter, store=0, prn=handlePackets)
 
